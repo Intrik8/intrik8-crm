87497338f20bef47c9a660b17659dfc8faa3faae
Jason Scherer <schereja@gmail.com>
2015-07-22 14:50:16 -0500

    Loading of modules based on configuration file working, TODO: Load CRUD options dynamically.

--- server.js
+++ server.js
@@ -9,7 +9,8 @@
 
 //Required
 
-console.log((new Date()).getTime())
+
+var beginning = (new Date()).getTime()
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -19,15 +20,30 @@ var systemConfig = require('./cfg/systemConfig.json');
 var pub = __dirname;
 var router = express.Router();
 var modulesEnabled = require('./lib/systemCalls').GetModulesInstalled;
+var modulesInstalled = [];
+console.log('Welcome to ' + systemConfig.name + '\n');
+/*Setup Controllers based on system config file, offers the ability to turn on
+  and off controller
+  Also creates a variable based on the name called (name)Controller.  Convention
+  needed for these modules.
+  */
+for(var i=0; i<modulesEnabled.length; i++){
+    try{
+      var controller = modulesEnabled[i]+'Controller';
+      eval(systemConfig.modules[i].name + "Controller = require('./controllers/" + controller+"')");
+      console.log('Loading Modules:\n\tLoaded: ' + systemConfig.modules[i].name);
+    }catch(exception){
+      console.log('\tModule: '+ systemConfig.modules[i].name + ' did not load correctly.');
+    }
 
-//Setup Controllers based on system config file, offers the ability to turn on and off controller
-for(var i in modulesEnabled){
-    var controller = systemConfig.modules[i].name+'Controller';
-    controller = require('./controllers/'+controller);
-    console.log('Loaded: ' + systemConfig.modules[i].name);
 }
 
+router.route('/api/customers')
+  .post(customerController.postCustomer);
 
 var app = express();
 app.use(logger('dev'));
-console.log((new Date()).getTime())
+console.log('Server loaded in: '+((new Date()).getTime()-beginning) + 'ms')
+var server = app.listen(1337);
+console.log('Server started at port \nCtrl+C to close');
+module.exports = router;
